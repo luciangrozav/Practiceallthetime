2ac6ac4a85b829ccaf508ab71ea3e265b7d1a29e
luciangrozav <luciangrozav01@gmail.com>
2021-05-25 17:47:42 +0300

    Initial commit

---
+++ src/main/java/org/fasttrackit/App.java
@@ -0,0 +1,17 @@
+package org.fasttrackit;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Lucian! Welcome to practice!" );
+    }
+
+    int[] list = {1, 2, 3, 4, 5, 6};
+
+    for (int x:list)
+    {
+        System.out.println(x);
+    }
+}
