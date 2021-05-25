40e90d7d3a790313f4b8296135f61f10648fe366
luciangrozav <luciangrozav01@gmail.com>
2021-05-25 17:56:10 +0300

    Practice-for enhanced(2)

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -8,11 +8,18 @@ public class App {
 
 
         int prod=1;
+        int prod2=1;
         int[] list = {1, 2, 3, 4, 5, 6};
 
         for (int x : list) {
             prod *=x;
         }
         System.out.println(prod);
+
+        for(int i=0; i< list.length; i++)
+        {
+            prod2*= list[i];
+        }
+        System.out.println(prod2);
     }
 }
